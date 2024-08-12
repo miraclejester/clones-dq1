b85111bbed3f09f926cbbf1f77b71754a10b1907
miraclejester <miraclejester413@gmail.com>
2024-08-11 23:17:55 -0400

    Continous movement on command window

--- battle/ui/command_window.gd
+++ battle/ui/command_window.gd
@@ -3,17 +3,27 @@ class_name CommandWindow
 
 signal selected(index: int)
 
+enum MoveState {
+	IDLE, CONTINOUS
+}
+
 @export var command_label_scene: PackedScene
+@export var start_wait_time: float = 0.2
+@export var move_wait_time: float = 0.1
 
 @onready var ui_window: NinePatchRect = $UIWindow
 @onready var margin_container: MarginContainer = $MarginContainer
 @onready var grid_container: GridContainer = %GridContainer
+@onready var continous_timer: Timer = %ContinousTimer
+
 
 var command_callables: Array[Callable]
 
 
 var selection_index: int = 0
 var menu_active: bool = false
+var move_state: MoveState = MoveState.IDLE
+var cont_input: String = ""
 
 var left_margin: int
 var right_margin: int
@@ -26,29 +36,49 @@ func _ready() -> void:
 	right_margin = margin_container.get_theme_constant("margin_right")
 	top_margin = margin_container.get_theme_constant("margin_top")
 	bottom_margin = margin_container.get_theme_constant("margin_bottom")
+	continous_timer.timeout.connect(on_contiunous_timeout)
 
 
 func _process(_delta: float) -> void:
 	if not menu_active:
 		return
-	
-	if Input.is_action_just_pressed("left"):
-		move_left()
-	elif Input.is_action_just_pressed("right"):
-		move_right()
-	elif Input.is_action_just_pressed("down"):
-		move_down()
-	elif Input.is_action_just_pressed("up"):
-		move_up()
-	
-	if Input.is_action_just_pressed("A"):
-		select()
-	elif Input.is_action_just_pressed("B"):
-		cancel()
+		
+	match move_state:
+		MoveState.IDLE:
+			if Input.is_action_just_pressed("left"):
+				cont_input = "left"
+				continous_timer.start(start_wait_time)
+				move_state = MoveState.CONTINOUS
+				move_left()
+			elif Input.is_action_just_pressed("right"):
+				cont_input = "right"
+				continous_timer.start(start_wait_time)
+				move_state = MoveState.CONTINOUS
+				move_right()
+			elif Input.is_action_just_pressed("down"):
+				cont_input = "down"
+				continous_timer.start(start_wait_time)
+				move_state = MoveState.CONTINOUS
+				move_down()
+			elif Input.is_action_just_pressed("up"):
+				cont_input = "up"
+				continous_timer.start(start_wait_time)
+				move_state = MoveState.CONTINOUS
+				move_up()
+			
+			if Input.is_action_just_pressed("A"):
+				select()
+			elif Input.is_action_just_pressed("B"):
+				cancel()
+		MoveState.CONTINOUS:
+			if Input.is_action_just_released(cont_input):
+				continous_timer.stop()
+				move_state = MoveState.IDLE
 
 
 func initialize_commands(commands: Array[CommandData], columns: int) -> void:
 	command_callables = []
+	move_state = MoveState.IDLE
 	for child in grid_container.get_children():
 		grid_container.remove_child(child)
 		child.queue_free()
@@ -130,3 +160,16 @@ func on_main_container_resized() -> void:
 	var width: int = int(grid_container.size.x) + left_margin + right_margin
 	var height: int = int(grid_container.size.y) + top_margin + bottom_margin
 	ui_window.size = Vector2(width, height)
+
+
+func on_contiunous_timeout() -> void:
+	match cont_input:
+		"left":
+			move_left()
+		"right":
+			move_right()
+		"down":
+			move_down()
+		"up":
+			move_up()
+	continous_timer.start(move_wait_time)
