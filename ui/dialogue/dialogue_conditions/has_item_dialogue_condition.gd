d3643283b5e802e96d8969dfd94a5335c36fd5d6
miraclejester <miraclejester413@gmail.com>
2024-08-27 23:02:03 -0400

    Charlock Castle

--- ui/dialogue/dialogue_conditions/has_item_dialogue_condition.gd
+++ ui/dialogue/dialogue_conditions/has_item_dialogue_condition.gd
@@ -4,4 +4,8 @@ class_name HasItemDialogueCondition
 @export var item: ItemData
 
 func evaluate(_window: DialogueWindow, params: Dictionary) -> bool:
-	return PlayerManager.hero.inventory.has_item(params.get("grant_item_item", item))
+	var i: ItemData = params.get("grant_item_item", item) as ItemData
+	if i is EquipmentData:
+		return PlayerManager.hero.equipment.get_equip(i.equipment_type) == i
+	else:
+		return PlayerManager.hero.inventory.has_item(params.get("grant_item_item", item))
